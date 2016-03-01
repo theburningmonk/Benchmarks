1f8e4064f31c082a7c3693244411085064466871
Yan Cui <theburningmonk@gmail.com>
2016-03-01 23:19:06 +0000

    - init commit

---
+++ src/Benchmarks/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Benchmarks")>]
+[<assembly: AssemblyProductAttribute("Benchmarks")>]
+[<assembly: AssemblyDescriptionAttribute("A collection of my benchmarks")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
